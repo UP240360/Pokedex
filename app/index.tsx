ffaf05b82d1bc7ce9b3e1fa636ace1320c08ede8
Daniel Nava <up240360@alumnos.upa.edu.mx>
2026-03-02 15:30:36 -0600

    add getPokemons fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,31 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry"
 
 export default function Index() {
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  const getPokemons = async () => {
+    try {
+      const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    const response = await fetch(URL, {
+      method: "GET",
+    });
+    if (response.ok) {
+      console.log("Request Ok");
+    } else {
+      console.log("Bad Request");
+    }
+  } catch (error) {
+    console.log("Error fetching pokemons:", error);
+  }
+    }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
